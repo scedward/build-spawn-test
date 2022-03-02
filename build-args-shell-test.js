e72406cdc871a7706b99c33656911168ec5977e9
Scott Edwards <scedward@cisco.com>
2022-03-02 13:16:05 -0500

    tweak output

--- build-args-shell-test.js
+++ build-args-shell-test.js
@@ -1,14 +1,13 @@
 const { spawnSync } = require('child_process');
 
-spawnSync('docker', ['build', '--tag', 'no-build-args-no-shell', '.'], { stdio: 'inherit' })
-spawnSync('docker', ['build', '--tag', 'build-args-no-shell', '--build-arg', 'FOO=$FOO', '.'], { stdio: 'inherit' })
-spawnSync('docker', ['build', '--tag', 'no-build-args-with-shell', '.'], { shell: '/bin/sh', stdio: 'inherit' })
-spawnSync('docker', ['build', '--tag', 'build-args-with-shell', '--build-arg', 'FOO=$FOO', '.'], { shell: '/bin/sh', stdio: 'inherit' })
-
-console.log('==========')
 console.log(`process.env.FOO is: ${process.env.FOO}`)
+console.log('building images...')
+spawnSync('docker', ['build', '--tag', 'no-build-args-no-shell', '.'])
+spawnSync('docker', ['build', '--tag', 'build-args-no-shell', '--build-arg', 'FOO=$FOO', '.'])
+spawnSync('docker', ['build', '--tag', 'no-build-args-with-shell', '.'], { shell: '/bin/sh' })
+spawnSync('docker', ['build', '--tag', 'build-args-with-shell', '--build-arg', 'FOO=$FOO', '.'], { shell: '/bin/sh' })
+
 console.log(`no-build-args-no-shell: ${spawnSync('docker', ['run', 'no-build-args-no-shell']).stdout}`)
 console.log(`build-args-no-shell: ${spawnSync('docker', ['run', 'build-args-no-shell']).stdout}`)
 console.log(`no-build-args-with-shell: ${spawnSync('docker', ['run', 'no-build-args-with-shell']).stdout}`)
 console.log(`build-args-with-shell: ${spawnSync('docker', ['run', 'build-args-with-shell']).stdout}`)
-
